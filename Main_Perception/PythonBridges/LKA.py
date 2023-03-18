2786767feceb253d5928f7cda1006e5964b32541
ThomasPlacide <47633912+ThomasPlacide@users.noreply.github.com>
2023-03-18 15:46:08 +0100

    [GROUPE PERCEPTION] Légère modif sur LKA / LKA_lib

--- Main_Perception/PythonBridges/LKA.py
+++ Main_Perception/PythonBridges/LKA.py
@@ -1,40 +1,15 @@
 
-import LKA_lib as L
 import rtmaps.types
 import numpy as np
 import rtmaps.core as rt 
 import rtmaps.reading_policy 
-from rtmaps.base_component import BaseComponent 
+from rtmaps.base_component import BaseComponent
+from LKA_lib import Pipeline 
 
-def Weightener(Image): 
-    '''
-    Pipeline pour détection et inscription de ligne sur un flux vidéo
-    '''
-    Gray_Image =[]
-    HoughLines = []
-    Cropped = []
-    WeightedImage = []
-    
-    Gray_Image = L.grayscale(Image)
-    Gray_Image = L.gaussian_blur(Image, 3)
 
-    lowTr = 150
-    upTr = 350
-    Gray_Image = L.canny(Gray_Image, lowTr, upTr)
-
-    # plt.imshow(Gray_Image, cmap='gray')
-    # Gray_Image.shape
-
-    trapeze = np.array( [[(0, 350), (600,350), (900, 540), (0, 540)]] )
-    Cropped = L.region_of_interest(Gray_Image, trapeze)
-    # plt.imshow(Cropped, cmap='gray')
-
-    HoughLines = L.hough_lines(Cropped, rho=2, theta=np.pi/6, threshold=1, min_line_len=4, max_line_gap=25)
-    # plt.imshow(HoughLines)
-
-    WeightedImage = L.weighted_img(HoughLines, Image, α=0.8, β=1., γ=0.)
-    
-    return WeightedImage
+#############
+#BLOC RTMAPS#
+#############
 
 class rtmaps_python(BaseComponent):
     def __init__(self):
@@ -54,6 +29,7 @@ class rtmaps_python(BaseComponent):
 
     # Core() is called every time you have a new input
     def Core(self):
+        img = self.inputs["Image"].ioelt.dat
         pass
 
        
